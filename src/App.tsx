4c7f96a3632a0b4df8ca06e2aa1591c24ebd5a30
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-08-20 14:19:42 +0900

    [upd] body

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import Table from "./Pool";
-import styled from "styled-components";
-import { poolDimension } from "./poolDimension";
-import Control, { controlWidth } from "./Control";
-import { generateAll } from "./drills";
-import { load } from "./storage";
+import React from "react";
+import Body from "./Body";
 
 function App(): React.ReactElement {
-  const exs = useMemo(generateAll, []);
-  const [idx, setIdx] = useState(0);
-  const { cue, object } = exs[idx];
-  const exerciseData = load();
-  const upadte = useCallback(() => {
-    const n = Math.floor(Math.random() * exs.length);
-    setIdx(n);
-  }, [exs.length, setIdx]);
-
-  useEffect(() => {
-    upadte();
-  }, [upadte]);
-
-  return (
-    <>
-      <Field>
-        <Table cueBall={cue} objectBall={object} />
-        <Control
-          cueBall={cue}
-          objectBall={object}
-          exerciseData={exerciseData}
-          exIndex={idx}
-          update={upadte}
-        />
-      </Field>
-    </>
-  );
+  return <Body />;
 }
 
 export default App;
-
-const Field = styled.div`
-  display: flex;
-  calc(${poolDimension.width + controlWidth}px + var(--basic-gap));
-`;
